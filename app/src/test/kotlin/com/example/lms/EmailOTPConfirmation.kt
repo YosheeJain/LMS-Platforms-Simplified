9d3f36becba3ab9d2ce5b1051173a37432c35c2c
Yoshee Jain <yosheejain@gmail.com>
2023-07-19 15:44:51 -0500

    created activities with boilerplate code for all respective xmls

---
+++ app/src/test/kotlin/com/example/lms/EmailOTPConfirmation.kt
@@ -0,0 +1,10 @@
+package com.example.lms
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EmailOTPConfirmation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
